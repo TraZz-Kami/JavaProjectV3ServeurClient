2a3106d50870f4f74cd701d0bbac87a2ff97249e
TraZz-Kami <58888326+TraZz-Kami@users.noreply.github.com>
2023-02-10 23:52:44 +0100

    Add files via upload

---
+++ LauncherLinux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -jar ./JavaProjectV3ServeurClient_1.jar
+read -p "Press enter to continue"
---
+++ launcher.bat
@@ -0,0 +1,6 @@
+@echo off
+
+java -jar ./JavaProjectV3ServeurClient_1.jar
+pause
+
+@echo on
